7cc053c8592e57a964ac4b6dafc83e2195517583
rth <rth@r-a-r.org>
2026-08-23 18:13:59 -0400

    Split 10 runs into two parallel batches to enable execution on standard hardware

--- inf-test.sh
+++ inf-test.sh
@@ -1,19 +1,32 @@
 #! /bin/bash
 
+WORKDIR='inf-test/scratch'
+
+[ -d ${WORKDIR} ]||{ mkdir -p ${WORKDIR} || exit 0 ; }
+
 run_and_compute(){
     MODEL=${1}
     TEST=${2}
-    python main.py ${MODEL} ${MODEL%%.yaml}-${TEST}.npz | tee ${MODEL%%.yaml}-${TEST}.log && python computeMI.py --feature-array=features --remove-first=100 --shuffle-iterations 1 --output ${MODEL%%.yaml}-${TEST}.json ${MODEL%%.yaml}-${TEST}.npz | tee -a ${MODEL%%.yaml}-${TEST}.log 
+    RUNNER=${WORKDIR}/$(basename ${MODEL%%.yaml}-${TEST})
+    python main.py ${MODEL} ${RUNNER}.npz | tee ${RUNNER}.log && python computeMI.py --feature-array=features --remove-first=100 --shuffle-iterations 1 --output ${MODEL%%.yaml}-${TEST}.json ${RUNNER}.npz | tee -a ${RUNNER}.log 
 }
 
 for MODEL in inf-test/*.yaml
 do
     pids=()
-    for TEST in $(seq 0 9)
+    for TEST in $(seq 0 4)
     do
        run_and_compute  ${MODEL} ${TEST} & pids+=($!)
     done
     for pid in ${pids[*]}; do wait $pid; done
+
+    pids=()
+    for TEST in $(seq 5 9)
+    do
+       run_and_compute  ${MODEL} ${TEST} & pids+=($!)
+    done
+    for pid in ${pids[*]}; do wait $pid; done
+
 done
 
-cd inf-test && python plot-all-runs.py
+#cd inf-test && python plot-all-runs.py
